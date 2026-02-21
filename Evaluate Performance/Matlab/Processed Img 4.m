5e11b5b5d0e615b24d43a6bc0fb5de791971404c
Fotechi <bushra840b@gmail.com>
2026-02-22 01:14:31 +0500

    Create Processed Img 4.m

---
+++ Evaluate Performance/Matlab/Processed Img 4.m
@@ -0,0 +1,23 @@
+fileID= fopen('Edges2.bin','rb');
+data= fread(fileID);
+% data=array;
+%% 
+% CONV = IP - FIL + ZP(lEFT+RIGHT) / ST  + 1
+imageWidth = 256 ;               %  510 No. of columns
+imageHeight = 255 ;              %  510 No. of rows
+numColor = 1;
+
+newData= uint8(zeros(imageWidth*imageHeight*numColor,1));
+l = 1 ;
+for i = 1:imageWidth 
+	for j= 1: imageHeight 
+		for k = 1 :numColor
+newData(l+(k-1)*(imageWidth*imageHeight)) = data(imageWidth*(j-1)*numColor+(i-1)*numColor+k);
+		end
+        l = l + 1 ;
+	end
+end
+fclose(fileID);
+finalData= reshape (newData, [imageHeight,imageWidth,numColor]);
+figure;
+imshow(uint8(finalData))
